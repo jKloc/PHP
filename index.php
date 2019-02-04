cb57db5d13f09b87486342883841c2b59103b320
Marcin Kloc <marcin.k.kloc@gmail.com>
2019-02-04 23:58:24 +0100

    Update index.php
    changed few HTML options, add functionality which get json file change it into object and show email adresses

--- index.php
+++ index.php
@@ -2,25 +2,33 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Emaile list</title>
+    <title>Emails list</title>
 </head>
-<body bgcolor = "gray">
-    <size="6" color="black">
-    <strong><p>list:</p></strong>
-
+<body>
+	<strong><h1>list of clients email adresses:</h1></strong>
 <?php
 
+	//autoload the appropriate class, all dependencies will be handle automatically
+	require_once 'vendor/autoload.php';		
+	// new instance of Client
+	$Client = new GuzzleHttp\Client([		
+		'headers' => ['User-Agent' => 'EmailReader']
+	]);
 
-//autoload the appropriate class, all dependencies will be handle automatically
-require_once 'vendor/autoload.php';
-// new instance of Client
-$Client = new GuzzleHttp\Client([
-'headers' => ['User-Agent' => 'EmailReader']
-]);
+	//get body from web-site with try catch block for catch exceptions
+    $response = $Client->request('GET', 'https://eie.jippioo.no/api/brokers');
+
+	//create an object
+	$response = json_decode($response->getBody()->getContents());
+    
+	$brokers = $response->data->brokers;
+	
+	//Loop for each broker object and echo broker name with email adress
+	foreach ($brokers as $broker){
+	    echo $broker->email . "<br><br>";
+	}
 
 ?>
 
 </body>
 </html>
-
-
